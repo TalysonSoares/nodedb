75b4b932b9ee7a2202537f2d2fde8acea4d20cba
TalysonSoares <talysonsoares40@gmail.com>
2022-11-01 11:13:11 -0300

    exportando

--- connection/index.js
+++ connection/index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 const config = require('../config/database');
 
-async function executarNoBanco() {
+async function executarNoBanco(query) {
     const conexao = await mysql.createConnectioN({
         host: config.DB_HOST,
         user: config.DB_USER,
@@ -9,7 +9,11 @@ async function executarNoBanco() {
         database: config.DB_NAME,
     });
 
-    const [results, ] = await conexao.execute();
+    const [results, ] = await conexao.execute(query);
 
     return results;
 }
+
+module.exports = {
+    executarNoBanco
+};
