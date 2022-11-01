6b51edbb7834195ce7606dea3c25f6f28d69809c
TalysonSoares <talysonsoares40@gmail.com>
2022-11-01 11:44:02 -0300

    adicionando commit

--- controllers/categoria/categoriaController.js
+++ controllers/categoria/categoriaController.js
@@ -2,8 +2,9 @@ const {executarNoBanco} = require('../../connection')
 
 
 async function listar() {
-    return await executarNoBanco('SELECT * FROM tb_categoria');
+    return await executarNoBanco('SELECT * FROM tb_categoria'); //('SELECT id, nome FROM tb_categoria');
 }
+
 
 module.exports = {
     listar,
