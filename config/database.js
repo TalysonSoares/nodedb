9795148c8afa325027de7c36d34e778f96df9e9a
TalysonSoares <talysonsoares40@gmail.com>
2022-11-01 11:02:33 -0300

    adicionando os dados da conexão

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASSWORD = '1234';
+const DB_NAME = 'db_comercio';
+
+module.exports = {
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME
+}
