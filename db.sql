b025a589046e648aadb7e8c500f231919afd2d75
TalysonSoares <talysonsoares40@gmail.com>
2022-11-01 10:46:02 -0300

    alterando arquivo

--- db.sql
+++ db.sql
@@ -1,2 +1,11 @@
 -- Criando banco de dados --
 CREATE DATABASE db_comercio;
+
+--Selecionando banco para ser usado --
+USE db_comercio;
+
+-- Criando tabela de categoria --
+CREATE TABLE tb_categoria (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+)
