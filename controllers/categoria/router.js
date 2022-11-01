feccd8d77baa466a65a0501982d30db27eca6e0a
TalysonSoares <talysonsoares40@gmail.com>
2022-11-01 10:24:32 -0300

    working

--- controllers/categoria/router.js
+++ controllers/categoria/router.js
@@ -1,2 +1,10 @@
 const controller = require('./categoriaController');
 const router = require('express').Router();
+
+router.get('/categoria', (req, res) => {
+    res.send(
+        controller.listar()
+    )
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = 8000;
+const categoriaRouter = require('./controllers/categoria/router');
 
 app.use(express.json());
 
@@ -9,7 +10,9 @@ app.get('/', (req, res) =>{
     res.send('Pagina inicial');
 }); 
 
+app.use('/',)
 
 app.listen(port, () => {
     console.log("API rodando no endereço http://localhost:8000");
 });
+
