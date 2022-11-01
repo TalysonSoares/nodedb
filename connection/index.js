94522e7dfb193cf7e807dcd8dfd1f7d71e571bc9
TalysonSoares <talysonsoares40@gmail.com>
2022-11-01 11:08:25 -0300

    atualizando

--- connection/index.js
+++ connection/index.js
@@ -1,2 +1,15 @@
 const mysql = require('mysql2/promise');
+const config = require('../config/database');
 
+async function executarNoBanco() {
+    const conexao = await mysql.createConnectioN({
+        host: config.DB_HOST,
+        user: config.DB_USER,
+        password: config.DB_PASSWORD,
+        database: config.DB_NAME,
+    });
+
+    const [results, ] = await conexao.execute();
+
+    return results;
+}
